61fe9cc6050d33d2dcb044226677118dc96e3098
francineleey-sudo <francineleey@gmail.com>
2025-08-11 01:28:55 +0800

    main_dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+// ignore_for_file: unused_local_variable
 
+import 'markclass.dart';
+import 'mikeclass.dart';
+import 'laikaclass.dart';
+import 'empressclass.dart';
+
+void main() {
+  var mark = Mark('Mark Angelo Arnoco', 'Calumpang, Naval, Biliran');
+  mark.describe();
+  var mikeclass = Mike('Mikee Defensor', 'Basak, Atipolo');
+  mikeclass.describe();
+  var laikaclass =
+      Laika('Laika Dela Cruz Salomon', 'Caraycaray, Naval, Biliran');
+  laikaclass.describe();
+  var empressclass =
+      Empress('Empress Joyce Monton', 'Sangalang Biliran, Biliran');
+  empressclass.describe();
+}
